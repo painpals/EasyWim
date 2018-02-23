7a0518c96c562088f0e95bc83eab93c0ece59e7b
Tyler Ziegler <taziegler5@gmail.com>
2018-02-23 14:22:57 -0800

    Create MainPage.py
    this is a wip

---
+++ MainPage.py
@@ -0,0 +1,113 @@
+#Created by Ryan Rubash and Tyler Ziegler
+# EasyWIM 1.0 2/22/2018
+#for flashing wim images into hard drives
+
+from Tkinter import *
+import tkFileDialog
+import subprocess
+
+
+main = Tk()
+
+# global vars
+wimlocation = StringVar()
+destLocation = StringVar()
+driveList = []
+
+
+def callpath():
+    global wimlocation
+    wimlocation.set(tkFileDialog.askopenfilename(parent=main, initialdir="/", title='Please select a directory'))
+
+
+def closescript():
+    exit()
+
+
+def scandestination():
+    global driveList
+    while len(driveList) > 0:
+        driveList.pop()
+
+    checkPhyDrives = subprocess.Popen("wmic diskdrive get index,model", stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+
+    for line in checkPhyDrives.stdout.readlines():
+        if "Index" in line:
+            continue
+        driveList.append(line.rstrip())
+        driveList = filter(None, driveList)
+    return driveList
+
+
+def startcopy():
+    global wimlocation
+    global destLocation
+
+    print(wimlocation.get())
+    print(destLocation)
+
+    closescript()
+
+
+def CurSelect(event):
+    global destLocation
+    widget = event.widget
+    selection=widget.curselection()
+    destLocation = widget.get(selection[0])
+
+
+def sourceframe():
+    # title
+    titleLabel = main
+    titleLabel.title("EasyWIM")
+
+    # Source text on top
+    fileMessageLabel = Label(main, text="Source:", anchor=NW, width=70)
+    fileMessageLabel.grid(row=0)
+
+    # entry box for file path
+    source = Entry(main, width=80, textvariable=wimlocation)
+    source.grid(row=1)
+
+    # select source button here
+    sourceButton = Button(main, width=20, text="Image Location...", command=callpath)
+    sourceButton.grid(row=1, column=1)
+
+
+def destinationframe():
+    global destLocation
+    # destination text here
+    destinationMessageLabel = Label(main, text="Destination:", anchor=NW, width=70)
+    destinationMessageLabel.grid(row=2)
+    # list of destinations found
+    destinationOptions = Listbox(main, width=80, selectmode=SINGLE)
+    destinationOptions.bind('<<ListboxSelect>>',CurSelect)
+    destinationOptions.grid(row=3)
+    scandestination()
+    for item in driveList:
+        destinationOptions.insert(END, item + " Drive")
+
+    destinationRefresh = Button(main, text="Refresh", width=20, anchor=NW, command=destinationframe)
+    destinationRefresh.grid(row=3, column=1)
+    destLocation = destinationOptions.get(ACTIVE)
+
+
+def optionframe():
+    # start/cancel container
+    optionContainer = Frame(main)
+    optionContainer.grid(row=4, column=1)
+
+    # insert start button
+    startButton = Button(optionContainer, width=10, text="Start", command=startcopy)
+    startButton.grid(row=0, column=0)
+
+    # insert cancel button
+    cancelButton = Button(optionContainer, width=10, text="Cancel", command=closescript)
+    cancelButton.grid(row=0, column=1)
+
+
+sourceframe()
+destinationframe()
+optionframe()
+
+main.mainloop()
