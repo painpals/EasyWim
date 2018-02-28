1f65afc2a345491a6fa08aa2868ebd2068a68309
Tyler Ziegler <taziegler5@gmail.com>
2018-02-28 10:36:02 -0800

    Update MainPage.py

--- MainPage.py
+++ MainPage.py
@@ -1,31 +1,34 @@
 # Created by Ryan Rubash and Tyler Ziegler
-# EasyWIM 0.6 2/22/2018
+# EasyWIM 1.0 2/22/2018
 # for flashing wim images into hard drives
 # written for python 2.7
 
 from Tkinter import *
 import tkFileDialog
 import tkMessageBox
-import subprocess
+import subprocess, string, os, uuid, time
+from shutil import copyfile
+
 
 
 main = Tk()
 
 # global vars
 isWim = False
-wimlocation = StringVar()
+wimLocation = StringVar()
 destLocation = StringVar()
 driveList = []
+dpartfilename = str(uuid.uuid4().hex) #this is the tempfile for diskpart
 
 
 def callpath():
     global isWim
-    global wimlocation
-    wimlocation.set(tkFileDialog.askopenfilename(parent=main, initialdir="/", title='Please select a directory'))
+    global wimLocation
+    wimLocation.set(tkFileDialog.askopenfilename(parent=main, initialdir="/", title='Select your .wim file'))
     # now checks path and will print error message if not a .wim file
-    if str(wimlocation.get()).endswith(".wim"):
+    if str(wimLocation.get()).endswith(".wim"):
         isWim = True
-    elif str(wimlocation.get()) is "":
+    elif str(wimLocation.get()) is "":
         isWim = False
     else:
         isWim = False
@@ -40,9 +43,7 @@ def scandestination():
     global driveList
     while len(driveList) > 0:
         driveList.pop()
-
-    checkPhyDrives = subprocess.Popen("wmic diskdrive get index,model", stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
+    checkPhyDrives = subprocess.Popen("wmic diskdrive get index,model,serialnumber", stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     for line in checkPhyDrives.stdout.readlines():
         if "Index" in line:
             continue
@@ -52,14 +53,88 @@ def scandestination():
 
 
 def startcopy():
-    global wimlocation
+    global wimLocation
     global destLocation
     global isWim
-
     if isWim == True:
-        print(wimlocation.get())
-        print(destLocation)
-
+        #print(wimLocation.get())
+        #print(destLocation)
+        ## Ryan can type code here- this is on start button
+        #CLEAN DISK
+        dpartfile = open(dpartfilename, "w")
+        #clean selected disk
+        dpartfile.write('select disk ' + str(destLocation[:1]))
+        dpartfile.write('\nclean')
+        dpartfile.close()
+        #EXECUTE THE DISKPART TO CLEAN THE SELECTED DISK NUMBER
+        os.system("diskpart /s " + dpartfilename)
+        os.remove(dpartfilename)
+        #DEFINING ALPHABET OF UNSUED VOLUMES
+        alphabet = []
+        for i in string.ascii_uppercase:
+            #alphabet.append(i + ':')
+            alphabet.append(i)
+        mounted_letters = subprocess.Popen("wmic logicaldisk get name", stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        for line in mounted_letters.stdout.readlines():
+            if "Name" in line:
+                continue
+            for letter in alphabet:
+                if letter in line:
+                    #print 'Deleting letter %s from free alphabet ' + letter
+                    alphabet.pop(alphabet.index(letter))
+        print alphabet
+        del alphabet[0:2] #this is to ensure there are a couple free letters not in use
+        if len(alphabet) < 3:
+            print "There are not enough free Volume Letters"
+            exit(2)
+        partitionWindows = alphabet[0]
+        del alphabet[0]
+        partitionRecovery = alphabet[0]
+        del alphabet[0]
+        partitionHidden = alphabet[0]
+        print "--- Volume letters for drive partitions..."
+        print "Windows - " + partitionWindows
+        print "Recovery - " + partitionRecovery
+        print "Hidden - " + partitionHidden
+        partitionWindows = partitionWindows[:1]
+        partitionRecovery = partitionRecovery[:1]
+        partitionHidden = partitionHidden[:1]
+        #CREATE DRIVE PARTITONS
+        dpartfile = open(dpartfilename, "w")
+        dpartfile.write('select disk ' + str(destLocation[:1]))
+        dpartfile.write('\nconvert gpt')
+        dpartfile.write('\ncreate partition primary size=440')
+        dpartfile.write('\nassign letter ' + partitionRecovery)
+        dpartfile.write('\nformat quick fs=ntfs label="Windows RE tools"')
+        dpartfile.write('\nset id="de94bba4-06d1-4d40-a16a-bfd50179d6ac"')
+        dpartfile.write('\ngpt attributes=0x8000000000000001')
+        dpartfile.write('\ncreate partition efi size 260')
+        dpartfile.write('\nassign letter ' + partitionHidden)
+        dpartfile.write('\nformat quick FS=FAT32')
+        dpartfile.write('\ncreate partition msr size 128')
+        dpartfile.write('\ncreate partition primary')
+        dpartfile.write('\nformat quick FS=NTFS LABEL="WINDOWS"')
+        dpartfile.write('\nassign letter ' + partitionWindows)
+        dpartfile.close()
+        #EXECUTE THE DISKPART TO CREATE PARTITIONS ON THE SELECTED DISK NUMBER
+        os.system("diskpart /s " + dpartfilename)
+        os.remove(dpartfilename)
+        #COPY UNATTEND FILE
+        os.system("copy unattend.xml "+ partitionWindows + ":\\")
+        #APPLY IMAGE
+        pathWim = wimLocation.get()
+        pathWim = pathWim.replace("/","\\")
+        os.system('dism /apply-image /imagefile:"' + pathWim + '" /index:1 /ApplyDir:' + partitionWindows + ':\\')
+        #MAKE BOOTABLE
+        os.system("bcdboot.exe " + partitionWindows + ":\\windows /s " + partitionHidden + ":")
+        #COPY DESKTOP FILES
+        if os.path.exists("DesktopFiles"):
+            from distutils.dir_util import copy_tree
+            print "Copying Desktop Files"
+            copy_tree("DesktopFiles", partitionWindows + ":\\Users\\Public\\Desktop")
+        print "Image Process Is Complete"
+        os.system("color 20") #this turns prompt green.. needs errorchecking
+        time.sleep(5)
         closescript()
     else:
         tkMessageBox.showerror(title="Error", message="File selected is either not a .wim file, or no file was selected")
@@ -82,7 +157,7 @@ def sourceframe():
     fileMessageLabel.grid(row=0)
 
     # entry box for file path
-    source = Entry(main, width=80, textvariable=wimlocation)
+    source = Entry(main, width=80, textvariable=wimLocation)
     source.grid(row=1)
 
     # select source button here
@@ -102,8 +177,7 @@ def destinationframe():
     scandestination()
     for item in driveList:
         destinationOptions.insert(END, item + " Drive")
-
-    destinationRefresh = Button(main, text="Refresh", width=20, anchor=NW, command=destinationframe)
+    destinationRefresh = Button(main, text="Refresh Drive List", width=20, command=destinationframe)
     destinationRefresh.grid(row=3, column=1)
     destLocation = destinationOptions.get(ACTIVE)
 
